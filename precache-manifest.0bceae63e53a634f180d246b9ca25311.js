89f43da6375e83ed3e964c28c40be8f0bd725cd6
dagon derik farias dantas <dagonderik@gmail.com>
2022-08-18 16:48:12 -0300

    Updates

---
+++ precache-manifest.0bceae63e53a634f180d246b9ca25311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f8916d7a6ecbf70cb8",
+    "url": "/static/css/main.0621d7eb.chunk.css"
+  },
+  {
+    "revision": "c9f8916d7a6ecbf70cb8",
+    "url": "/static/js/main.c9f8916d.chunk.js"
+  },
+  {
+    "revision": "fbd0924dd8ee322e9bc2",
+    "url": "/static/js/1.fbd0924d.chunk.js"
+  },
+  {
+    "revision": "50a4ef364e715d46567a",
+    "url": "/static/css/2.b56bb24a.chunk.css"
+  },
+  {
+    "revision": "50a4ef364e715d46567a",
+    "url": "/static/js/2.50a4ef36.chunk.js"
+  },
+  {
+    "revision": "e3530f4ae72efdf7e1c1",
+    "url": "/static/js/runtime~main.e3530f4a.js"
+  },
+  {
+    "revision": "2d360d363044d38ef1fcdc02c819b37f",
+    "url": "/index.html"
+  }
+];
