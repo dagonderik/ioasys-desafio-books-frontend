828657a66aa1bf32f1d4de3d359de3a01faa9ee2
dagon derik farias dantas <dagonderik@gmail.com>
2022-08-18 17:00:07 -0300

    Updates

---
+++ precache-manifest.555ad0cf45c9ca8c9a96ac4833186b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f8916d7a6ecbf70cb8",
+    "url": "/ioasys-desafio-books-frontend/static/css/main.0621d7eb.chunk.css"
+  },
+  {
+    "revision": "c9f8916d7a6ecbf70cb8",
+    "url": "/ioasys-desafio-books-frontend/static/js/main.c9f8916d.chunk.js"
+  },
+  {
+    "revision": "fbd0924dd8ee322e9bc2",
+    "url": "/ioasys-desafio-books-frontend/static/js/1.fbd0924d.chunk.js"
+  },
+  {
+    "revision": "50a4ef364e715d46567a",
+    "url": "/ioasys-desafio-books-frontend/static/css/2.b56bb24a.chunk.css"
+  },
+  {
+    "revision": "50a4ef364e715d46567a",
+    "url": "/ioasys-desafio-books-frontend/static/js/2.50a4ef36.chunk.js"
+  },
+  {
+    "revision": "73a3215c33e65cd2456d",
+    "url": "/ioasys-desafio-books-frontend/static/js/runtime~main.73a3215c.js"
+  },
+  {
+    "revision": "02232a183a2ed121a6b7f1b2d3a8ad69",
+    "url": "/ioasys-desafio-books-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bceae63e53a634f180d246b9ca25311.js"
+  "/ioasys-desafio-books-frontend/precache-manifest.555ad0cf45c9ca8c9a96ac4833186b73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ioasys-desafio-books-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
