a46668db3f4c66b56aa7633717b78098c3b42903
dagon derik farias dantas <dagonderik@gmail.com>
2022-08-24 19:31:43 -0300

    Added styleguide folder

---
+++ styleguide/precache-manifest.883c488b6692f9d2c7c881fd42d15c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe11d6aca22df6e46da",
+    "url": "static/css/main.98b9bcf2.chunk.css"
+  },
+  {
+    "revision": "afe11d6aca22df6e46da",
+    "url": "build/main.afe11d6a.js"
+  },
+  {
+    "revision": "ee55d7e35dbbd26a2513",
+    "url": "build/bundle.ee55d7e3.js"
+  },
+  {
+    "revision": "f005196309b09da43a1d",
+    "url": "build/2.f0051963.js"
+  },
+  {
+    "revision": "5ed0fb9c01753f0ab5a55288d1bb4f66",
+    "url": "static/media/background.5ed0fb9c.png"
+  },
+  {
+    "revision": "b2574a3eb8b134181d6ed8e7ac8b340c",
+    "url": "build/2.f0051963.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9a644811850822a57c6c8627f98611",
+    "url": "index.html"
+  }
+];
